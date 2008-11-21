80cb434e1db6f35f2066774821a3cd45fe8b13ba
Chad Humphries <chad@spicycode.com>
2008-11-20 22:48:00 -0500

    Clarifying pending example without a block description

--- examples/lib/micronaut_example.rb
+++ examples/lib/micronaut_example.rb
@@ -2,6 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + "/../example_helper")
 
 describe Micronaut do
   
-  it "should not do anything"
+  it "should allow pending examples by not supplying a block"
   
 end
