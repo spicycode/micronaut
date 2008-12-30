204899f980a7455cf942cdb42c5e6008d289502c
Rob Sanheim <rsanheim@gmail.com>
2008-12-30 02:04:19 -0500

    code review

--- lib/micronaut/world.rb
+++ lib/micronaut/world.rb
@@ -17,6 +17,7 @@ module Micronaut
       
       if filter
         @behaviours_to_run = filter_behaviours
+        # RJS the line below is very confusing and looks like a dupe of the line below it -- is it dead code or does it serve a purpose?
         behaviours_to_run.size == 0 && Micronaut.configuration.run_all_when_everything_filtered?
         if @behaviours_to_run.size == 0 && Micronaut.configuration.run_all_when_everything_filtered?
           puts "No behaviours were matched by #{filter.inspect}, running all"
