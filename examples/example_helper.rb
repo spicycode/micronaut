812c5cbc8e95a571586e2a808f2e59161ccebef9
Chad Humphries <chad@spicycode.com>
2009-01-01 09:17:52 -0500

    Clean up editor detection for color

--- examples/example_helper.rb
+++ examples/example_helper.rb
@@ -7,7 +7,7 @@ gem :mocha
 
 require File.expand_path(File.dirname(__FILE__) + "/resources/example_classes")
 
-module Micronaut  
+module Micronaut
   module Matchers
     def fail
       raise_error(::Micronaut::Expectations::ExpectationNotMetError)
@@ -23,8 +23,12 @@ def remove_last_describe_from_world
   Micronaut.world.behaviours.pop
 end
 
+def not_in_editor?
+  ['TM_MODE', 'EMACS', 'VIM'].all? { |k| !ENV.has_key?(k) }
+end
+
 Micronaut.configure do |c|
   c.mock_with :mocha
-  c.color_enabled = !ENV.has_key?('TM_MODE')
+  c.color_enabled = not_in_editor?
   c.filter_run :focused => true
 end
