9f11c04ddbd992454fbe09406be04ade962a7240
Chad Humphries <chad@spicycode.com>
2009-02-07 13:54:45 -0500

    Minor example helper cleanup

--- examples/example_helper.rb
+++ examples/example_helper.rb
@@ -34,9 +34,7 @@ def not_in_editor?
 end
 
 Micronaut.configure do |c|
-  # c.formatter = :documentation
   c.mock_with :mocha
   c.color_enabled = not_in_editor?
   c.filter_run :focused => true
-  c.profile_examples = true
 end
